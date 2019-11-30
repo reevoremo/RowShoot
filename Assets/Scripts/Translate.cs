6d92fd1e2ec69b8812c260d219a41ef233ced0d0
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2019-11-30 02:58:12 +0100

    Scripts for movement and turning

---
+++ Assets/Scripts/Translate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour
+{
+
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        Transform t = GetComponent<Transform>();
+
+        Vector3 movement = new Vector3();
+
+        movement.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        t.Translate(movement);
+    }
+}
